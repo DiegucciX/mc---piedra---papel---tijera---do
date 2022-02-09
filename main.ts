468e8c089e1e9b6ba2889a919d4258f91fefa5b4
DiegucciX <98874031+DiegucciX@users.noreply.github.com>
2022-02-09 08:50:04 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
-basic.forever(function () {
+input.onGesture(Gesture.Shake, function () {
+    if (jugada == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (jugada == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else if (jugada == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
+radio.onReceivedString(function (receivedString) {
 	
+})
+let jugada = 0
+radio.setGroup(10)
+basic.forever(function () {
+    jugada = randint(1, 3)
 })
